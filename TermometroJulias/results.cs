75c6cf724597cf0a9c54378a12cf4ecba34f1a8c
julias <jsantana23julia@gmail.com>
2024-04-18 09:02:52 -0300

    18/04

---
+++ TermometroJulias/results.cs
@@ -0,0 +1,11 @@
+
+namespace Termometrojulias;
+public class results
+
+{
+     public int Temp {get;set;}
+  
+    public string description {get;set;}
+    public string condition_code {get;set;}
+
+}
