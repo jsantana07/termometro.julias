f512b4bc3572b3881bfe8a54ef8d0401a8fdc13b
julias <jsantana23julia@gmail.com>
2024-04-19 11:05:51 -0300

    julias 19/04

--- TermometroJulias/MainPage.xaml.cs
+++ TermometroJulias/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace TermometroJulias;
+﻿using Termometrojulias;
+
+namespace TermometroJulias;
 
 public partial class MainPage : ContentPage
 {
@@ -7,8 +9,26 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-	}
 
+		Resultados = new results();
+		PreencherTela();
+
+	}
+		void PreencherTela()
+		{
+			labelTemp.Text= Resultados.temp.ToString();
+			labelSky.Text= Resultados.description;
+			labelCidade.Text= Resultados.city;
+			labelChuva.Text= Resultados.rain.ToString();
+			labelHumidade.Text= Resultados.humidity.ToString();
+			labelAmanhecer.Text= Resultados.sunrise;
+			labelAnoitecer.Text= Resultados.sunset;
+			labelForcawind.Text= Resultados.wind_speedy.ToString();
+			labelDirecaoWind.Text= Resultados.wind_direction;
+			labelMoonFase.Text= Resultados.moon_phase;
+
+
+		}
 	
 }
 
